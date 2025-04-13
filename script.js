78d3ced46879ee3ab1048064ceb14b5f5cfbbb59
Ulf-KaPunkt <ulf.knobloch@gmx.de>
2025-04-13 19:24:56 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,54 +1,46 @@
 
-const songs = {
-  "0-1": "Back in Black – AC/DC",
-  "0-2": "Are You Gonna Be My Girl – Jet",
-  "1-1": "Zombie – The Cranberries",
-  "1-2": "Rolling in the Deep – Adele",
-  "2-1": "Wonderwall – Oasis",
-  "2-2": "Shallow – Lady Gaga & Bradley Cooper",
-  "3-1": "Smells Like Teen Spirit – Nirvana",
-  "Z-1": "Highway to Hell – AC/DC"
-};
-
-const male = ["0-1", "0-2", "2-1", "3-1", "Z-1"];
+const songs = {'0-1': 'Zombie – The Cranberries', '0-2': 'Like the way I do – Melissa Etheridge', '0-3': 'All the small things – Blink 182', '0-4': 'Narcotic – Liquido', '0-5': 'Summer of 69 – Bryan Adams', '0-6': 'Boulevard of broken dreams – Green Day', '0-7': 'Ironic – Alanis Morissette', '0-8': 'You oughta know – Alanis Morissette', '0-9': 'Teenage Dirtbag – Wheatus', '0-10': 'What’s up – 4 Non Blondes', '1-1': 'Sex on fire – Kings of Leon', '1-2': 'Take me out – Franz Ferdinand', '1-3': 'Seven Nation Army – The White Stripes', '1-7': 'Valerie – Amy Winehouse', '1-8': 'Stupid Girls – Pink', '1-12': 'Shallow – Lady Gaga & Bradley Cooper', '2-1': 'Lonely Boy – The Black Keys', '2-5': 'Runaway Train – Soul Asylum', '2-6': 'I was made for loving you – Kiss', '2-7': 'Highway to Hell – AC/DC', '2-8': 'When I come around – Green Day', '2-9': 'Mr. Brightside – The Killers', '2-11': 'Somebody to love – Jefferson Airplane', '2-13': 'Friday I’m in love – The Cure', '2-14': 'Chasing Cars – Snow Patrol', '2-15': 'Wonderwall – Oasis', '2-16': 'Denkmal – Wir sind Helden', '2-17': 'Sunday Bloody Sunday – U2', '3-1': 'Westerland – Die Ärzte', '3-4': 'You give love a bad name – Bon Jovi', '3-8': 'Mein Herz brennt – Rammstein', '3-11': 'Engel – Rammstein', '3-12': 'Nothing else matters – Metallica', 'Z-2': 'Highway to Hell – AC/DC'};
+const maennlich = ['0-4', '0-5', '0-6', '0-12', '1-1', '1-2', '1-3', '1-7', '1-8', '1-12', '2-1', '2-5', '2-6', '2-7', '2-8', '2-9', '2-11', '2-13', '2-14', '2-15', '2-16', '2-17', '3-1', '3-4', '3-8', '3-11', '3-12', 'Z-2'];
 const sets = {
-  "0": ["0-1", "0-2"],
-  "1": ["1-1", "1-2"],
-  "2": ["2-1", "2-2"],
-  "3": ["3-1"],
-  "Z": ["Z-1"]
+  "0": Object.keys(songs).filter(k => k.startsWith("0-")),
+  "1": Object.keys(songs).filter(k => k.startsWith("1-")),
+  "2": Object.keys(songs).filter(k => k.startsWith("2-")),
+  "3": Object.keys(songs).filter(k => k.startsWith("3-")),
+  "Z": Object.keys(songs).filter(k => k.startsWith("Z-"))
 };
+const weiblich = Object.keys(songs).filter(k => !maennlich.includes(k));
+let last15 = [];
 
-let history = [];
-
-function updateSetDropdown() {
+function handleFilterChange() {
   const filter = document.getElementById("filter").value;
-  document.getElementById("setSelect").style.display = filter === "set" ? "inline-block" : "none";
+  document.getElementById("setSelect").style.display = filter === "sets" ? "inline-block" : "none";
 }
 
-function drawSong() {
+function pickRandomSong() {
   const filter = document.getElementById("filter").value;
-  let pool = Object.keys(songs);
+  let pool = [];
 
-  if (filter === "male") {
-    pool = male;
-  } else if (filter === "female") {
-    pool = pool.filter(k => !male.includes(k));
-  } else if (filter === "set") {
-    const selected = document.getElementById("setSelect").value;
-    pool = sets[selected] || [];
+  if (filter === "alle") {
+    pool = Object.keys(songs);
+  } else if (filter === "maennlich") {
+    pool = maennlich;
+  } else if (filter === "weiblich") {
+    pool = weiblich;
+  } else if (filter === "sets") {
+    const selectedSet = document.getElementById("setSelect").value;
+    pool = sets[selectedSet] || [];
   }
 
-  const available = pool.filter(k => !history.includes(k));
+  const available = pool.filter(id => !last15.includes(id));
   if (available.length === 0) {
-    alert("Alle Songs wurden kürzlich gespielt. Speicher wird zurückgesetzt.");
-    history = [];
+    alert("Alle Songs kürzlich gespielt. Liste wird zurückgesetzt.");
+    last15 = [];
     return;
   }
 
-  const choice = available[Math.floor(Math.random() * available.length)];
-  document.getElementById("songDisplay").textContent = songs[choice];
+  const picked = available[Math.floor(Math.random() * available.length)];
+  last15.push(picked);
+  if (last15.length > 15) last15.shift();
 
-  history.push(choice);
-  if (history.length > 15) history.shift();
+  document.getElementById("output").textContent = songs[picked];
 }
